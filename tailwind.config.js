cdb6894a8bb7d13c0bb1df422a92b16b7c46e7bc
Oludire Damilare <oludire@gmail.com>
2025-02-07 18:46:37 +0100

    campaign pt-1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
-/** @type {import('tailwindcss').Config} */ 
-module.exports = { 
-  content: [ 
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ], 
-  theme: { 
-    extend: {}, 
-    colors: { 
-      "sidebarColor": "#666D80",
-      "sidebarTitle": "#A4ABB8"
-      
-    } 
-  }, 
-  plugins: [], 
-}
-
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Manrope'],
+      serif: ['inter'],
+      mono: ['Poppins']
+    },
+    extend: {
+      colors: {
+        primary: {
+          50: '#A4ABB8',
+          100: '#666D80'
+        }
+      }
+    },
+    plugins: []
+  }
+};
