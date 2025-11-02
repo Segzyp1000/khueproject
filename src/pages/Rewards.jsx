868a405bf80429fb98661b3009281f10a57fec72
Segun Olowoyeye <segunolowoyeye@gmail.com>
2025-11-02 05:46:45 +0100

    chamged text

--- src/pages/Rewards.jsx
+++ src/pages/Rewards.jsx
@@ -1,11 +1,20 @@
 
 import Layout from "../components/Layout";
+import { IoMdHappy } from "react-icons/io";
 
 
 const Rewards = () => {
   return (
     <Layout title="Rewards">
-      <div className="p-5">work in progres.....</div>
+      <div className="flex ">
+        <h2 className="font-bold text-4xl ">Coming soon</h2>
+        <div className="flex gap-3 font-bold  px-3 text-yellow-500 text-3xl mt-3">
+        <IoMdHappy  />
+          <IoMdHappy  />
+            <IoMdHappy  />
+            </div>
+
+        </div>
     </Layout>
   );
 };
